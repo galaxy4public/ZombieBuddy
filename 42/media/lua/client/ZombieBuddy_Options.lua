4ad3535162b16fe65924ef20614c2a8bebe51d9e
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-05-04 17:20:35 +0300

    watermark opacity modoptions

---
+++ 42/media/lua/client/ZombieBuddy_Options.lua
@@ -0,0 +1,22 @@
+local zbOptions = PZAPI.ModOptions:create("ZombieBuddy", "ZombieBuddy")
+
+local watermarkOpacity = zbOptions:addSlider(
+    "watermarkOpacity",
+    "UI_ZB_WatermarkOpacity",
+    0.0, 1.0, 0.05, 0.4
+)
+
+local function onChangeWatermarkOpacity(self, value)
+    if ZombieBuddy and ZombieBuddy.Watermark and ZombieBuddy.Watermark.setAlpha then
+        ZombieBuddy.Watermark.setAlpha(value)
+    end
+end
+
+watermarkOpacity.onChange      = onChangeWatermarkOpacity
+watermarkOpacity.onChangeApply = onChangeWatermarkOpacity
+
+Events.OnMainMenuEnter.Add(function()
+    if ZombieBuddy and ZombieBuddy.Watermark and ZombieBuddy.Watermark.setAlpha then
+        ZombieBuddy.Watermark.setAlpha(watermarkOpacity:getValue())
+    end
+end)
